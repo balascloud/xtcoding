8a5cd43639ea52c21c1bba6fb72a53aeac11ce40
balascloud <balasweb2@gmail.com>
2021-07-16 19:55:54 +0530

    Undefined error for limit

--- src/app/xtc/xtc.services.ts
+++ src/app/xtc/xtc.services.ts
@@ -13,12 +13,13 @@ export class XTCAPIService {
     }
 
     getData(_params: any) {
+        let _limit = ( _params['limit']) ?  _params['limit'] : 100;
         let params: any = {
             launch_success: _params['launch_success'],
             land_success: _params['land_success'],
             launch_year: _params['launch_year'],
             limit: _params['limit']
         };
-        return this.http.get<any>(this.serviceUrl + '?limit=' + params['limit'] + '&launch_success=' + params['launch_success'] + '&land_success=' + params['land_success'] + '&launch_year=' + params['launch_year']);
+        return this.http.get<any>(this.serviceUrl + '?limit=' + _limit + '&launch_success=' + params['launch_success'] + '&land_success=' + params['land_success'] + '&launch_year=' + params['launch_year']);
     }
 }
